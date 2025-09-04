098b053f5e66c1eb0da5a9a9758566d7e126ff36
Gerardo Salazar <62206583@fsv.cuni.cz>
2025-09-04 16:57:04 -0600

    Add code implementation/execution that retrieves the matrix calculations

--- Code.py
+++ Code.py
@@ -228,3 +228,43 @@ class Matrix:
         res = Matrix(self.to_list())
         result = res.rref_and_inverse()
         return result[1]
+
+A = [[2,1,7],
+     [4,1,5], 
+     [-6,-2,9]]
+
+A = Matrix(A)
+
+B = [[5,2,3],
+     [-4,-1,6], 
+     [7,5,2]]
+
+B = Matrix(B)
+
+
+A.transpose().print()
+print("Are matrices equal?", A==B)
+print("1. row, 2. column of matrix A:", A[0][1])
+
+(A+B).print()
+(A-B).print()
+(A*B).print()
+B.row_swap(0, 1)
+B.print()
+B.row_multiply(1, 2)
+B.print()
+
+print('Test REF, _REF')
+
+A.REF().print() # creates REF from a copy of matrix A, A is unchanged
+A.print()
+A._REF().print() # A prevede do REF
+A.print()
+
+
+print("Test RREF, inverse")
+
+B.RREF().print() #Reduce matrices to REF (Row Echelon Form) and RREF (Reduced Row Echelon Form)  creates RREF from a copy of matrix B, B is unchanged
+B.print()
+B.inverse().print() # return an inverse from a copy of matrix B, B is unchanged
+B.print()
